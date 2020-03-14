e69965cc5dccf5094d9eea81bf01ae1b203443d2
yiyexingyu <2958029539@q.com>
2020-03-14 08:44:44 +0800

    初步打通API流程

---
+++ app.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+# @File    : app.py
+# @Time    : 2020/3/13 23:18
+# @Author  : 一叶星羽
+# @Email   : h0670131005@gmail.com
+# @Software: PyCharm
+
+
+from flask import Flask,jsonify
+from flask_restful import reqparse, Resource, Api
+from bookInfo.allBookInfo import getNovelListFromBiquge
+
+app = Flask(__name__)
+app.config["JSON_AS_ASCII"]=False
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument("count", type=int, required=False)
+
+class GetNovelFromBiquge(Resource):
+
+    def get(self):
+        arg = parser.parse_args()
+        if "count" in arg:
+            count = arg.get("count")
+        else:
+            count = 0
+
+        resData = getNovelListFromBiquge(count)
+        return jsonify(resData)
+
+api.add_resource(GetNovelFromBiquge, "/novel/biquge/")
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ bookInfo/allBookInfo.py
@@ -0,0 +1,89 @@
+# -*- encoding: utf-8 -*-
+# @File    : allBookInfo.py
+# @Time    : 2020/3/13 23:19
+# @Author  : 一叶星羽
+# @Email   : h0670131005@gmail.com
+# @Software: PyCharm
+
+
+import re
+from lxml import etree
+import requests
+from urllib import request
+
+
+def getNovelInfo(novelUrl: str, filterRe: str, process=None) -> list:
+    """
+    根据小说的url地址和匹配规则filterRe，爬取小说的相关信息。process是额外处理函数，
+    爬取相关信息后，对这些信息进行额外的处理。比如补全小说的每个章节的url。
+    :param novelUrl: 小说相关信息的url
+    :param filterRe: 相关信息匹配规则-制作表达式
+    :param process: 额外的处理函数,必须接收一个参数。
+    """
+
+    # 向服务器发送请求，并获取返回的html页面
+    html = request.urlopen(novelUrl).read().decode("utf-8")
+    # 匹配的正则表达式
+    novelInfo = re.findall(filterRe, html)
+
+    if process:
+        novelInfo = process(novelInfo)
+    return novelInfo
+
+
+def novelFromBiquge():
+    url1 = "http://www.xbiquge.la/15/15003/"
+    req = "<dd><a href='(.*?)' >(.*?)</a></dd>"
+
+    chapters = getNovelInfo(url1, req)
+    return chapters
+
+
+def getNovelListByOriginName(originName: str):
+    originInfo = {
+        "笔趣阁":"http://www.xbiquge.la/xiaoshuodaquan/",
+    }
+
+
+def getNovelListFromBiquge(novelCount: int = 0) -> dict:
+    url = "http://www.xbiquge.la/xiaoshuodaquan/"
+
+    # 向服务器发送请求，并获取返回的html页面
+    html = requests.get(url)
+    novelInfoList = {}
+
+    novelHtmlList = etree.HTML(html.text)
+
+    novelTags = novelHtmlList.xpath("//*[@id='main']/div[@class='novellist']/h2/text()")
+    novelTyped = novelHtmlList.xpath("//*[@id='main']/div[@class='novellist']")
+    novelId = 0
+
+    for i in range(len(novelTags)):
+        novelTag = re.findall("(.*?)小说", novelTags[i].replace('、', ''))
+        novelLinks = novelTyped[i].xpath("./ul/li/a/@href")
+
+        # 爬取每一本小说的详细信息
+        for novelLink in novelLinks:
+            infoHtml = requests.get(novelLink)
+            infoHtml.encoding = infoHtml.apparent_encoding
+            novelInfoHtml = etree.HTML(infoHtml.text)
+
+            novelInfo = {"tag": novelTag, "link": novelLink}
+            novelInfo["title"] = novelInfoHtml.xpath("//*[@id='info']/h1/text()")[0]
+            novelInfo["author"] = novelInfoHtml.xpath("//*[@id='info']/p[1]/text()")[0].split("：")[-1]
+            novelInfo["state"] = "完结" # 需要额外处理
+            novelInfo["intro"] = novelInfoHtml.xpath("//*[@id='intro']/p[2]/text()")[0].strip()
+            novelInfo["update"] = novelInfoHtml.xpath("//*[@id='info']/p[3]/text()")[0].split("：")[-1]
+            novelInfo["cover"] = novelInfoHtml.xpath("//*[@id='fmimg']/img/@src")
+
+            novelInfoList[novelId] = novelInfo
+            novelId += 1
+
+            if novelCount > 0 and novelId >= novelCount:
+                return novelInfoList
+
+    return novelInfoList
+
+
+if __name__ == '__main__':
+    getNovelListFromBiquge()
